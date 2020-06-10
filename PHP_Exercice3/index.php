9381ee08b2b6904677b7d4d6e2229d5e8b6d8385
unknown <jules.gioux@gmail.com>
2020-06-10 10:48:18 +0200

    ex3

--- PHP_Exercice3/index.php
+++ PHP_Exercice3/index.php
@@ -1,14 +1,25 @@
+<?php
+    if (isset($_COOKIE['id']) && isset($_COOKIE['pwd']))
+    {
+        $formError = TRUE;
+        header('Location: result.php');
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Partie 8 Exercice 2</title>
+    <title>PHP Partie 8 Exercice 3</title>
 </head>
 <body>
     <form action="result.php" method="post">^
         <fieldset>
             <legend>Connection :</legend>
+            <?= (isset($_GET['formError']) && $_GET['formError'] == TRUE) ? 'Les champs de formulaires n\'ont pas été correctement rempli' : '' ?>
+            <br>
             <label for="id">Identifiant :</label>
             <input type="text" name="id" id="id">
             <label for="pwd">Password :</label>
--- PHP_Exercice3/result.php
+++ PHP_Exercice3/result.php
@@ -1,21 +1,21 @@
 <?php 
-    if ((!isset($_POST['id']) || !isset($_POST['pwd'])) && (!isset($_COOKIE['id']) || !isset($_COOKIE['pwd']))) {
-        header('Location: index.php');
-        exit();
-    }
-
-    if ((isset($_POST['id']) && isset($_POST['pwd'])) && (!empty($_POST['id']) && !empty($_POST['pwd']))) {
-        $id = $_POST['id'];
-        $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-        if (!isset($_COOKIE['id']) && !isset($_COOKIE['pwd'])) {        
+    $connected = FALSE;
+    if ((isset($_POST['id']) && !empty($_POST['id'])) && (isset($_POST['pwd']) && !empty($_POST['pwd']))) {
+        if (!isset($_COOKIE['id']) && !isset($_COOKIE['pwd'])) {
+            $id = htmlspecialchars($_POST['id']);
+            $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);        
             setcookie("id", $id, time()+3600, "/", "php-partie8");
             setcookie("pwd", $pwd, time()+3600, "/", "php-partie8");
             header('Location: result.php');
             exit();
         }      
-    } else {
-        header('Location: index.php');
+    } elseif (!isset($_COOKIE['id']) || !isset($_COOKIE['pwd']))
+    {
+        $formError = TRUE;
+        header('Location: index.php?formError=' . $formError);
         exit();
+    } else {
+        $connected = TRUE;
     }
 
 ?>
@@ -27,11 +27,9 @@
     <title>PHP Partie 8 Exercice 2 résultat</title>
 </head>
 <body>
-    <?php var_dump($_COOKIE) ?>
-    <pre>
-        <?php print_r($_COOKIE) ?>
-    </pre>
     <p>
+    <b style="color:red"><?= ($connected) ? 'CONNECTED !' : 'NOT CONNECTED !' ?></b>
+    <br>
     identifiant : <?= $_COOKIE['id'] ?>
     <br>
     Mot de passe : <?= $_COOKIE['pwd'] ?>
